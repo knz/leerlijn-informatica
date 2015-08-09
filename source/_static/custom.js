ad0922e327bc1459c392d51024fcd819302a2ded
Raphael 'kena' Poss <knz@thaumogen.net>
2015-08-09 17:32:04 +0200

    Add Jargon file quotes to entries.

--- source/_static/custom.js
+++ source/_static/custom.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
 
+    $('#contents').after(
+        "<div class='icontainer'><span class='head'>Bonus: definitie uit de <a href='http://www.catb.org/jargon/html/index.html'>Jargon File</a></span><br/>" +
+        "<iframe id='fortune' src='//csa.science.uva.nl/fortune/fortune.cgi' /></div>" );
+
+    ifr = $("#fortune")
+    var sz = 0;
+    var shown = false;
+    ifr.load(function() {
+        setInterval(function() {
+            sz = $("#fortune").contents().height();
+            if (shown) {
+                $("#fortune").height(sz);
+            } else {
+                $("#fortune").height("3em");
+            }
+        }, 30);
+    });
+    $('.icontainer').click(function() {
+        $(this).children(".head").toggleClass("open");
+        shown = !shown;
+    });
+
     $('.utitle').parent().addClass('utitle-parent');
     $('.dtitle').parent().addClass('dtitle-parent');
     $('.ttitle').parent().addClass('ttitle-parent');
