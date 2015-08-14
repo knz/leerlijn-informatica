0c4a66c093c4dba9920d9d5fabfdbba73ac9481d
Raphael 'kena' Poss <r.c.poss@uva.nl>
2015-08-14 12:16:47 +0200

    fix typo

--- source/conf.py
+++ source/conf.py
@@ -133,7 +133,7 @@ html_theme = 'haiku'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Leerlijn informaticavaaardigheden'
+html_title = 'Leerlijn informaticavaardigheden'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = 'Leerlijn informaticavaardigheden'
